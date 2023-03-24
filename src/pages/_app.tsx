7144db0801a5b3e9f7e5fc0dbe84b40dc04e7280
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2023-03-24 14:01:39 -0300

    feat: config theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from "styled-components"
+import { summerTheme } from "@/styles/theme-config";
+import { GlobalStyles } from "@/styles/theme-config";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={summerTheme}>
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </ThemeProvider>
 }
---
+++ src/styles/theme-config.ts
@@ -0,0 +1,39 @@
+import {createGlobalStyle, DefaultTheme} from "styled-components"
+
+export interface Theme extends DefaultTheme {
+  gradient: string
+  primary: string
+  secondary: string
+  tertiary: string
+  primaryDark: string
+  secondaryDark: string,
+  fontColor: string
+}
+
+export const summerTheme: Theme = {
+  gradient: 'linear-gradient(180deg, #BFDAE6 0%, #FCF0DD 50%, #FACF9D 100%);',
+  primary: '#C8DEEF',
+  secondary: '#FBF0DD',
+  tertiary: '#FACF9D',
+  primaryDark: '#2E7BA2',
+  secondaryDark: '#DF9010',
+  fontColor: '#444444'
+}
+
+export const borealTheme: Theme = {
+  gradient: 'linear-gradient(180deg, #304968 0%, #36C1A0 52.6%, #4650BD 100%);',
+  primary: '#C8DEEF',
+  secondary: '#FBF0DD',
+  tertiary: '#FACF9D',
+  primaryDark: '#2E7BA2',
+  secondaryDark: '#DF9010',
+  fontColor: '#444444'
+}
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    background: ${({ theme }) => theme.gradient};
+    color: ${({ theme }) => theme.fontColor};
+    height: 100vh;
+  }
+`
