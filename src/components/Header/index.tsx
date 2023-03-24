a0fa930f2089f3fd67465f8c2fbb3c6d1f310aa7
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2023-03-24 15:15:57 -0300

    feat: add header for 768px layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,15 @@ export default function Header() {
   return (
     <S.Header>
       <Image src={'/images/logo-white.png'} alt="Logo da CodeChella" width={234} height={64} />
-      <Image src={'/images/icons/menu.svg'} alt="Menu icon" width={32} height={40} />
+      <Image src={'/images/icons/menu.svg'} alt="Menu icon" width={32} height={40} className="menu" />
+      <S.Menu>
+        <ul>
+          <li>A experiência</li>
+          <li>Mapa de Setores</li>
+          <li>Informações</li>
+          <li>Ingresso</li>
+        </ul>
+      </S.Menu>
     </S.Header>
   );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -4,8 +4,40 @@ export const Header = styled.header`
   height: 128px;
   width: 100%;
   background-color: ${({ theme }) => theme.primaryDark};
+  font-weight: 700;
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 0 2rem;
+
+  @media (min-width: 768px) {
+    height: 200px;
+    flex-direction: column;
+    justify-content: center;
+
+    font-size: 20px;
+
+    .menu {
+      display: none;
+    }
+  }
+`;
+
+export const Menu = styled.nav`
+  color: #fff;
+  display: none;
+
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+
+    ul {
+      display: flex;
+      width: 100%;
+      justify-content: space-evenly;
+      margin-top: 3rem;
+    }
+  }
 `;
