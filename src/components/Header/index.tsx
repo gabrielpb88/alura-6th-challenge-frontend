ca3e48f7113a42b39b9c0d1f10e9c78c22b7458d
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2023-03-24 15:33:01 -0300

    feat: add header for large devices

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,12 +7,12 @@ export default function Header() {
       <Image src={'/images/logo-white.png'} alt="Logo da CodeChella" width={234} height={64} />
       <Image src={'/images/icons/menu.svg'} alt="Menu icon" width={32} height={40} className="menu" />
       <S.Menu>
-        <ul>
+        <S.Ul>
           <li>A experiência</li>
           <li>Mapa de Setores</li>
           <li>Informações</li>
           <li>Ingresso</li>
-        </ul>
+        </S.Ul>
       </S.Menu>
     </S.Header>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,9 +2,11 @@ import styled from 'styled-components';
 
 export const Header = styled.header`
   height: 128px;
+  min-width: 360px;
   width: 100%;
   background-color: ${({ theme }) => theme.primaryDark};
   font-weight: 700;
+  font-size: 20px;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -15,11 +17,16 @@ export const Header = styled.header`
     flex-direction: column;
     justify-content: center;
 
-    font-size: 20px;
-
     .menu {
       display: none;
     }
+  }
+
+  @media (min-width: 1440px) {
+    height: 128px;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 0 150px;
   }
 `;
 
@@ -32,12 +39,24 @@ export const Menu = styled.nav`
     justify-content: center;
     align-items: center;
     width: 100%;
+  }
 
-    ul {
-      display: flex;
-      width: 100%;
-      justify-content: space-evenly;
-      margin-top: 3rem;
-    }
+  @media (min-width: 1440px) {
+    align-items: center;
+  }
+`;
+
+export const Ul = styled.ul`
+  @media (min-width: 768px) {
+    display: flex;
+    width: 100%;
+    justify-content: space-evenly;
+    margin-top: 3rem;
+  }
+
+  @media (min-width: 1440px) {
+    margin-top: 0;
+    justify-content: flex-end;
+    gap: 3rem;
   }
 `;
